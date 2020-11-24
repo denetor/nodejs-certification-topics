03dfc2c6748aeeacec1af83b5f76986f087c3462
denetor <denetor@gmail.com>
2020-11-24 15:55:07 +0100

    First readable stream creation and piping to stdout

--- src/streams/streams.js
+++ src/streams/streams.js
@@ -1,7 +1,21 @@
 module.exports = {
 
-    helloStreams: function () {
-        return 'streams.helloStreams()';
+	/**
+	 * First readable stream creation and piping to stdout
+	 *
+	 * @param req
+	 * @param res
+	 */
+    helloStreams: function(req, res) {
+	    var Readable = require('stream').Readable;
+	    var readStream = new Readable();
+	    readStream.push('hello');           // add items to stream
+	    readStream.push(' ');
+	    readStream.push('nodejs');
+	    readStream.push(' ');
+	    readStream.push('streams!\n');
+	    readStream.push(null);              // tell the stream it's done
+	    readStream.pipe(process.stdout);    // tell the consumer to read buffered items
     }
 
 };
